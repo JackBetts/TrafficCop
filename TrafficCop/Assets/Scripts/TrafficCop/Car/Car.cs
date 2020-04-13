eab460b643b15574a52b0e5e2392546e239b6cbb
Jack Betts <42355170+JackBetts@users.noreply.github.com>
2020-04-13 23:02:15 +0100

    Basic input and car movement

---
+++ TrafficCop/Assets/Scripts/TrafficCop/Car/Car.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using InputSamples.Gestures;
+using UnityEngine;
+
+namespace TrafficCop.Car
+{
+    public class Car : MonoBehaviour
+    {
+        public float moveSpeed; 
+        public float lateralMoveDistance;
+        public float forwardMoveDistance; 
+        public void CarMoveAttempt(SwipeInput input)
+        {
+            Vector2 direction = FinalSwipeValue(input.SwipeDirection); 
+            Debug.Log("Car Received swipe: X: " + direction.x + " Y: " + direction.y);
+
+            if (CheckCanMove(direction))
+            {
+                StartCoroutine(MoveCar(new Vector3(direction.x * lateralMoveDistance, 0, direction.y * forwardMoveDistance)));
+            }
+        }
+
+        private IEnumerator MoveCar(Vector3 direction)
+        {
+            Vector3 targetPosition = transform.position + direction;
+            float step = moveSpeed * Time.deltaTime; 
+            
+            while (transform.position != targetPosition)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+                yield return null; 
+            }
+        }
+
+        private bool CheckCanMove(Vector2 direction)
+        {
+            Vector3 rayDirection = new Vector3(direction.x, 0, direction.y);
+            RaycastHit hit;
+            
+            Debug.DrawLine(transform.position, transform.position + rayDirection * 10);
+            if (Physics.Raycast(transform.position, transform.position + rayDirection, out hit, 10))
+            {
+                if (hit.collider.GetComponent<Car>())
+                {
+                    return false;
+                }
+            }
+
+            return true; 
+        }
+
+        private Vector2 FinalSwipeValue(Vector2 swipeInput)
+        {
+            Vector2 tempInput = swipeInput;
+
+            if (tempInput.x > -0.5f && tempInput.x < 0.5f)
+            {
+                tempInput.x = 0; 
+            }
+            if (tempInput.y > -0.5f && tempInput.y < 0.5f)
+            {
+                tempInput.y = 0; 
+            }
+            if (tempInput.x > 0.5)
+            {
+                tempInput.x = 1; 
+            }
+            else if (tempInput.x < -0.5)
+            {
+                tempInput.x = -1;
+            }
+            
+            if (tempInput.y > 0.5)
+            {
+                tempInput.y = 1; 
+            }
+            else if (tempInput.y < -0.5)
+            {
+                tempInput.y = -1;
+            }
+            
+            return tempInput; 
+        }
+    }
+}
---
+++ TrafficCop/Assets/Scripts/TrafficCop/Controllers/SwipeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using InputSamples.Gestures;
+using UnityEngine;
+using TrafficCop.Car; 
+
+namespace TrafficCop.Controllers
+{
+    public class SwipeController : MonoBehaviour
+    {
+        [SerializeField] private GestureController gestureController; 
+        private readonly Dictionary<int, Car.Car> swipeMapping = new Dictionary<int, Car.Car>();
+
+        public Camera mainCam; 
+
+        private void OnEnable()
+        {
+            gestureController.Pressed += GestureControllerOnPressed;
+            gestureController.PotentiallySwiped += GestureControllerOnPotentiallySwiped;
+            gestureController.Swiped += GestureControllerOnSwiped;
+        }
+
+        private void OnDisable()
+        {
+            gestureController.Pressed -= GestureControllerOnPressed;
+            gestureController.PotentiallySwiped -= GestureControllerOnPotentiallySwiped;
+            gestureController.Swiped -= GestureControllerOnSwiped;
+        }
+
+        private void GestureControllerOnSwiped(SwipeInput obj)
+        {
+            Car.Car swipedCar;
+            if (!swipeMapping.TryGetValue(obj.InputId, out swipedCar))
+            {
+                return;
+            }
+
+            swipeMapping.Remove(obj.InputId);
+            swipedCar.CarMoveAttempt(obj);
+        }
+
+        private void GestureControllerOnPotentiallySwiped(SwipeInput obj)
+        {
+           
+        }
+
+        private void GestureControllerOnPressed(SwipeInput obj)
+        {
+            swipeMapping.Remove(obj.InputId);
+
+            RaycastHit hit;
+            Ray ray = mainCam.ScreenPointToRay(obj.EndPosition);
+            if (Physics.Raycast(ray, out hit, 500f))
+            {
+                if (hit.collider.GetComponent<Car.Car>())
+                {
+                    Car.Car hitCar = hit.collider.GetComponent<Car.Car>();
+                    swipeMapping[obj.InputId] = hitCar; 
+                }
+            }
+        }
+    }
+}
