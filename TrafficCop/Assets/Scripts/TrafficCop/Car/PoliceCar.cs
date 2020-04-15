bbb10cc7d0b85ced79ab81bc45f6ff8210353ef7
Jack Betts <42355170+JackBetts@users.noreply.github.com>
2020-04-15 23:14:08 +0100

    Checking for win

---
+++ TrafficCop/Assets/Scripts/TrafficCop/Car/PoliceCar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine; 
+
+namespace TrafficCop.Car
+{
+    public class PoliceCar : Car
+    {
+        public Transform raycastStart;
+        public float raycastLength;
+
+        private bool hasWon;
+        private void Start()
+        {
+            //InvokeRepeating(nameof(CheckForWin), 1, 1);
+        }
+
+        private void Update()
+        {
+            if (CheckForWin() && !hasWon)
+            {
+                hasWon = true;
+                Debug.Log("Check for win :: WE WIN WOOOOO"); 
+            }
+        }
+
+        private bool CheckForWin()
+        {
+            if (Physics.Raycast(raycastStart.position, Vector3.forward, out RaycastHit hit, raycastLength))
+            {
+                if (hit.collider)
+                {
+                    Debug.Log("Check for win :: hit collider :: " + hit.collider.name);
+                    return false;
+                }
+            }
+            
+            return true;
+        }
+    }
+}
